8977fe1bb9c5a028430a3954eec98b13a0359b28
René Majewski <rene.majewski@gmx.de>
2016-04-07 13:42:49 +0200

    Die SQL-Abfragen für die Tabelle "section" sind implementiert.

--- src/db/query/Section.java
+++ src/db/query/Section.java
@@ -7,29 +7,132 @@ package db.query;
  *
  */
 public class Section implements QueryInterface {
-
+	/**
+	 * Gibt die Datenbank-Abfrage zurück, die die Tabelle "section" erzeugt.
+	 * 
+	 * @return Datenbank-Abfrage, um die Tabelle "section" zu erzeugen.
+	 */
 	@Override
 	public String createTable() {
-		// TODO Auto-generated method stub
-		return null;
+		return "CREATE TABLE IF NOT EXISTS 'section' (" +
+				"'id' INTEGER PRIMARY KEY AUTOINCREMENT, " +
+				"'name' TEXT);";
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um ein neues Geschäft in die Tabelle
+	 * "section" zu speichern. Den Namen des neuen Geschäfts wird mit einen
+	 * <b>?</b> markiert.
+	 * 
+	 * @return Datenbank-Abfrage, um ein neues Geschäft zu erstellen.
+	 */
 	@Override
 	public String insert() {
-		// TODO Auto-generated method stub
-		return null;
+		return "INSERT INTO 'section' ('name') VALUES ('?');";
+	}
+	
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um ein neues Geschft in die Tabelle
+	 * "section" zu speichern. Diese Abfrage enthält bereits den Namen. Wird
+	 * <b>null</b> als Parameter übergeben, so wird als Platzhalter für den
+	 * Namen ein <b>?</b> eingefügt.
+	 * 
+	 * @param name Name des neuen Geschäfts.
+	 * 
+	 * @return Datenbank-Abfrage, um ein neues Geschäft zu erstellen.
+	 */
+	public String insert(String name) {
+		String ret = insert();
+		
+		if (name != null && !name.isEmpty()) {
+			ret = ret.replace("?", name);
+		}
+
+		return ret;
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um ein Geschäft aus der Tabelle
+	 * "section" zu löschen. Wurde eine ID größer <b>-1</> angegeben, so
+	 * ist diese bereits in der Rückgabe enthalten. Wurde als ID
+	 * <b>-1</b> angegeben, so ist für die ID der Platzhalter <b>?</b>
+	 * vorgesehen.
+	 * 
+	 * @param id ID des Datensatzes, der gelöscht werden soll.
+	 * 
+	 * @return Datenbank-Abfrage um den angegeben Datensatz zu löschen.
+	 */
 	@Override
 	public String delete(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		// Abfrage enthält Platzhalter
+		String ret = "DELETE FROM 'section' WHERE id = ?";
+		
+		// Platzhalter mit einer ID ersetzen?
+		if (id > -1)
+			ret = ret.replace("?", new String("\"" + id + "\""));
+		
+		// Abfrage zrück geben
+		return ret;
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um ein Geschäft in der Tabelle
+	 * "section" zu ändern. Wurde eine ID größer <b>-1</b> angegeben, so
+	 * wird die ID in die Abfrage aufgenommen. Wurde als ID <b>-1</b>
+	 * angegeben, wird für die ID ein <b>?</b> als Platzhalter in die
+	 * Datenbankabfrage übernommen.
+	 * 
+	 * @param id ID des Datensatzes, der geändert werde soll.
+	 * 
+	 * @return Datenbank-Abfrage, um den angegebenen Datensatz zu ändern.
+	 */
 	@Override
 	public String update(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		// Abfrage zum ändern des angegebenen Datensatzes
+		StringBuilder ret = new StringBuilder("UPDATE 'section' SET name = '?' WHERE id = ?");
+		
+		if (id > -1)
+			ret.replace(ret.lastIndexOf("?"), ret.lastIndexOf("?") + 1, "\"" + id + "\"");
+		
+		// Abfrage zurück geben
+		return ret.toString();
+	}
+	
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um ein Geschäft in der Tabelle
+	 * "section" zu ändern. Wurde eine ID größer <b>-1</b> angegeben, so wird
+	 * die ID in die Abfrage aufgebommen. Wurde als ID <b>-1</b> angegeben,
+	 * wird für die ID ein <b>?</b> als Platzhalter in die Datenbank-Abfrage
+	 * übernommen. Wird ein leerer String als Name angegeben, so wird für den
+	 * Namen ebenfalls ein <b>?</b> als Platzhalter in der Abfrage übernommen.
+	 * Wird ein Name angegeben, so wird der Name in die Abfrage übernommen.
+	 * 
+	 * @param id ID des Datensatzes, der geändert werden soll.
+	 * 
+	 * @param name Neuer Name des Geschäfts
+	 * 
+	 * @return Datenbank-Abfrage, um den angegeben Datensatz zu ändern.
+	 */
+	public String update(int id, String name) {
+		// Abfrage zum ändern des angegebenen Datensatzes
+		StringBuilder ret = new StringBuilder(update(id));
+		
+		if (name != null && !name.isEmpty())
+			ret.replace(ret.indexOf("?"), ret.indexOf("?") + 1, name);
+		
+		// abfrage zurück geben
+		return ret.toString();
+	}
+
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um alle Datensätze der Tabelle "section"
+	 * aufzulisten. Die Datensätze werden noch ihren IDs aufsteigend sortiert.
+	 * 
+	 * @return Datenbank-Abfrage, um alle Datensätze zurück zu geben.
+	 */
+	@Override
+	public String select() {
+		return "SELECT id, name FROM section ORDER BY id ASC;";
 	}
 	
 }
--- src/test/db/query/TestSection.java
+++ src/test/db/query/TestSection.java
@@ -5,20 +5,292 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import db.query.Section;
+import test.TestHelper;
+
 /**
+ * Enthält die Tests, um die Klasse {@link db.query.Selection} zu testen.
  * 
  * @author René Majewski
- *
  */
-public class TestSection {
+public class TestSection extends TestHelper {
+	/**
+	 * Speichert die Instanz der Datenbank-Abfragen der Tabelle "section"
+	 */
+	private static Section _section;
 
+	/**
+	 * Initalisiert die einzelnen Tests 
+	 */
 	@Before
 	public void setUp() throws Exception {
+		_section = new Section();
 	}
-
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#createTable()} keine
+	 * leere Zeichenkette liefert.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented"); // TODO
+	public void testCreateTableReturnNotNull() {
+		assertStringIsNotNull(_section.createTable());
 	}
 
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#insert()} keine leere
+	 * Zeichenkette liefert.
+	 */
+	@Test
+	public void testInsertReturnNotNull() {
+		assertStringIsNotNull(_section.insert());
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#insert()} ein
+	 * Fragezeichen enthält.
+	 */
+	@Test
+	public void testInsertHasAQuery() {
+		assertEquals(1, frequency(_section.insert(), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#insert(String)} keine
+	 * leere Zeichenkette liefert, auch wenn der Parameter <b>null</b> ist.
+	 */
+	@Test
+	public void testInsertStringWithNullAsParameterReturnNotNull() {
+		assertStringIsNotNull(_section.insert(null));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#insert(String)} ein
+	 * Fragezeichen enhält, wenn der Parameter <b>null</b> ist.
+	 */
+	@Test
+	public void testInsertStringWithNullAsParameterHasOneQuery() {
+		assertEquals(1, frequency(_section.insert(null), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#insert(String)} keine
+	 * leere Zeichenkette liefert.
+	 */
+	@Test
+	public void testInsertStringReturnNotNull() {
+		assertStringIsNotNull(_section.insert("test"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#insert(String)} kein
+	 * Fragezeichen zurück gibt.
+	 */
+	@Test
+	public void testInsertStringHasNoQuery() {
+		assertEquals(0, frequency(_section.insert("test"), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#delete(int)} keine
+	 * leere Zeichenkette liefert.
+	 */
+	@Test
+	public void testDeleteReturnNotNull() {
+		assertStringIsNotNull(_section.delete(100));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#delete(int)} kein
+	 * Fragezeichen in der Rückgabe enthält.
+	 */
+	@Test
+	public void testDeleteHasNoQuery() {
+		assertEquals(0, frequency(_section.delete(100), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#delete(int)} die
+	 * übergebene ID in der Rückgabe enthält.
+	 */
+	@Test
+	public void testDeleteHasId() {
+		assertEquals(1, frequency(_section.delete(100), "100"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#delete(int)} eine
+	 * leere Zeichenkette liefert, wenn der Parameter <b>-1</b> ist.
+	 */
+	@Test
+	public void testDeleteWithNullAsParameterReturnNull() {
+		assertStringIsNotNull(_section.delete(-1));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#delete(int)} ein
+	 * Fragezeichen in der Rückgabe enthält, wenn der Parameter <b>-1</b> ist.
+	 */
+	@Test
+	public void testDeleteWithNullAsParameterHasNoQuery() {
+		assertEquals(1, frequency(_section.delete(-1), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#delete(int)} keine
+	 * ID in der Rückgabe enthält, wenn der Parameter <b>-1</b> ist.
+	 */
+	@Test
+	public void testDeleteWithNullAsParameterHasId() {
+		assertEquals(0, frequency(_section.delete(-1), "100"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int)} keine leere
+	 * Zeichenkette liefert.
+	 */
+	@Test
+	public void testUpdateReturnNotNull() {
+		assertStringIsNotNull(_section.update(100));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int)} ein
+	 * Fragezeichen in der Rückgabe enthält.
+	 */
+	@Test
+	public void testUpdateHasOneQuery() {
+		assertEquals(1, frequency(_section.update(100), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int)} die
+	 * übergebene ID in der Rückgabe enthält.
+	 */
+	@Test
+	public void testUpdateHasId() {
+		assertEquals(1, frequency(_section.update(100), "100"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int)} keine leere
+	 * Zeichenkette liefert, auch wenn als Parameter -1 übergeben wird
+	 */
+	@Test
+	public void testUpdateMinusOneAsParameterReturnNotNull() {
+		assertStringIsNotNull(_section.update(-1));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int)} zwei
+	 * Fragezeichen in der Rückgabe enthält, wenn der PArameter -1 ist.
+	 */
+	@Test
+	public void testUpdateMinusOneAsParameterHasOneQuery() {
+		assertEquals(2, frequency(_section.update(-1), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int)} keine
+	 * ID in der Rückgabe enthält, wenn der Parameter -1 ist.
+	 */
+	@Test
+	public void testUpdateMinusOneAsParameterHasNoId() {
+		assertEquals(0, frequency(_section.update(-1), "-1"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)}
+	 * keine leere Zeichenkette liefert.
+	 */
+	@Test
+	public void testUpdateStringReturnNotNull() {
+		assertStringIsNotNull(_section.update(100, "name"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)}
+	 * kein Fragezeichen in der Rückgabe enthält.
+	 */
+	@Test
+	public void testUpdateStringHasNoQuery() {
+		assertEquals(0, frequency(_section.update(100, "test"), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int String)} die
+	 * übergebene ID in der Rückgabe enthält.
+	 */
+	@Test
+	public void testUpdateStringHasId() {
+		int id = 100;
+		assertEquals(1, frequency(_section.update(id, "test"), new String("\"" + id + "\"")));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)} den
+	 * übergebenen Namen enthält.
+	 */
+	@Test
+	public void testUpdateStringHasName() {
+		String test = new String("test");
+		assertEquals(1, frequency(_section.update(100, test), test));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)}
+	 * keine leere Zeichenkette liefert, der Parameter für die ID ist -1.
+	 */
+	@Test
+	public void testUpdateStringMinusOneAsParameterReturnNotNull() {
+		assertStringIsNotNull(_section.update(-1, "name"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)}
+	 * kein Fragezeichen in der Rückgabe enthält, der Parameter für die ID ist
+	 * -1.
+	 */
+	@Test
+	public void testUpdateStringMinusOneAsParameterHasOneQuery() {
+		assertEquals(1, frequency(_section.update(-1, "test"), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int String)} die
+	 * übergebene ID in der Rückgabe nicht enthält. Der Parameter ist -1.
+	 */
+	@Test
+	public void testUpdateStringMinusOneAsParamterHasNoId() {
+		int id = -1;
+		assertEquals(0, frequency(_section.update(id, "test"), new String("\"" + id + "\"")));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)}
+	 * kein Fragezeichen in der Rückgabe enthält. Der Parameter für den Namen
+	 * is <b>null</b>
+	 */
+	@Test
+	public void testUpdateStringNullAsParameterHasOneQuery() {
+		assertEquals(1, frequency(_section.update(100, null), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#update(int, String)}
+	 * kein Fragezeichen in der Rückgabe enthält. Der Parameter für den Namen
+	 * is <b>null</b>. Der Parameter für die ID ist -1.
+	 */
+	@Test
+	public void testUpdateStringMinusOneAndNullAsParametersHasTwoQueries() {
+		assertEquals(2, frequency(_section.update(-1, null), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.Section#select()} keine
+	 * leere Zeichenkette liefert.
+	 */
+	@Test
+	public void testSelectReturnNotNull() {
+		assertStringIsNotNull(_section.select());
+	}
 }
