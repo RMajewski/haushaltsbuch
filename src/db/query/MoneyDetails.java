9ad92cb73666899bde1464c53290eb0182dd3925
René Majewski <rene.majewski@gmx.de>
2016-04-09 10:47:21 +0200

    Standart-Abfragen für die Tabelle "money_details" hinzugefügt."

--- src/db/query/MoneyDetails.java
+++ src/db/query/MoneyDetails.java
@@ -8,34 +8,96 @@ package db.query;
  */
 public class MoneyDetails implements QueryInterface {
 
+
+	/**
+	 * Gibt die Datenbank-Abfrage zurück, die die Tabelle "money_details"
+	 * erzeugt.
+	 * 
+	 * @return Datenbank-Abfrage, um die Tabelle "money_details" zu erzeugen.
+	 */
 	@Override
 	public String createTable() {
-		// TODO Auto-generated method stub
-		return null;
+		return "CREATE TABLE IF NOT EXISTS 'money' (" +
+				"'id' INTEGER PRIMARY KEY AUTOINCREMENT, " +
+				"'moneyid' INTEGER NOT NULL, " +
+				"'categoryid' INTEGER NOT NULL, " +
+				"'sectionid' INTEGER NOT NULL, " +
+				"'money' DOUBLE, " +
+				"'comment' TEXT)";
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um einen neuen Eintrag in der Tabelle
+	 * "money_details" zu erzeugen. Für die 'moneyid', 'categoryid',
+	 * 'sectionid', das Geld und die Beschreiung wird jeweils ein "?" als
+	 * Platzhalter genutzt.
+	 *
+	 * 
+	 * @return Datenbank-Abfrage, um einen neuen Eintrag in "money_details" zu
+	 * erzeugen
+	 */
 	@Override
 	public String insert() {
-		// TODO Auto-generated method stub
-		return null;
+		return "INSERT INTO 'money_details' ('moneyid', 'categoryid', 'sectionid', 'money', 'comment') VALUES (?, ?, ?, ?, ?);";
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um einen Datensatz aus der Tabelle
+	 * "money_details" zu löschen. Wurde eine ID größer <b>-1</> angegeben, so
+	 * ist diese bereits in der Rückgabe enthalten. Wurde als ID
+	 * <b>-1</b> angegeben, so ist für die ID der Platzhalter <b>?</b>
+	 * vorgesehen.
+	 * 
+	 * @param id ID des Datensatzes, der gelöscht werden soll.
+	 * 
+	 * @return Datenbank-Abfrage um den angegeben Datensatz zu löschen.
+	 */
 	@Override
 	public String delete(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		// Abfrage enthält Platzhalter
+		String ret = "DELETE FROM 'money_details' WHERE id = ?";
+		
+		// Platzhalter mit einer ID ersetzen?
+		if (id > -1)
+			ret = ret.replace("?", new String("\"" + id + "\""));
+		
+		// Abfrage zrück geben
+		return ret;
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um einen Datensatz in der Tabelle
+	 * "money_details" zu ändern. Wurde ein ID größer <b>-1</b> angegeben, so
+	 * wird die ID in die Abfrage aufgenommen. Wurde als ID <b>-1</b>
+	 * angegeben, wird für die ID ein <b>?</b> als Platzhalter in die
+	 * Datenbankabfrage übernommen.
+	 * 
+	 * @param id ID des Datensatzes, der geändert werde soll.
+	 * 
+	 * @return Datenbank-Abfrage, um den angegebenen Datensatz zu ändern.
+	 */
 	@Override
 	public String update(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		// Abfrage zum ändern des angegebenen Datensatzes
+		StringBuilder ret = new StringBuilder("UPDATE 'money' SET date = ?, inout = ?, comment = '?' WHERE id = ?");
+		
+		// TODO Diese Abfrage-Bedingung als Methode implementieren
+		if (id > -1)
+			ret.replace(ret.lastIndexOf("?"), ret.lastIndexOf("?") + 1, String.valueOf(id));
+		
+		// Abfrage zurück geben
+		return ret.toString();
 	}
 
+	/**
+	 * Erzeugt die Datenbank-Abfrage, um alle Datensätze der Tabelle "money"
+	 * aufzulisten. Die Datensätze werden noch ihren IDs aufsteigend sortiert.
+	 * 
+	 * @return Datenbank-Abfrage, um alle Datensätze zurück zu geben.
+	 */
 	@Override
 	public String select() {
-		// TODO Auto-generated method stub
-		return null;
+		return "SELECT id, moneyid, categoryid, sectionid, money, comment FROM money_details ORDER BY id ASC";
 	}
 
 }
--- src/test/db/query/TestMoneyDetails.java
+++ src/test/db/query/TestMoneyDetails.java
@@ -6,13 +6,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import db.query.MoneyDetails;
+import test.TestHelper;
 
 /**
  * Testet die Klasse {@link db.query.MoneyDetails}
  * 
  * @author René Majewski
  */
-public class TestMoneyDetails {
+public class TestMoneyDetails extends TestHelper{
 	/**
 	 * Speichert die Instanz der Datenbank-Abfragen der Tabelle 'money_details'
 	 */
@@ -27,10 +28,148 @@ public class TestMoneyDetails {
 	public void setUp() throws Exception {
 		_moneyDetails = new MoneyDetails();
 	}
-
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#createTable()} keine
+	 * leere Zeichenkette liefert.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented"); // TODO
+	public void testCreateTableReturnNotNull() {
+		assertStringIsNotNull(_moneyDetails.createTable());
 	}
 
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#insert()} keine leere
+	 * Zeichenkette liefert.
+	 */
+	@Test
+	public void testInsertReturnNotNull() {
+		assertStringIsNotNull(_moneyDetails.insert());
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#insert()} fünf
+	 * Fragezeichen enthält.
+	 */
+	@Test
+	public void testInsertHasFifeQuery() {
+		assertEquals(5, frequency(_moneyDetails.insert(), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#delete(int)} keine
+	 * leere Zeichenkette liefert.
+	 */
+	@Test
+	public void testDeleteReturnNotNull() {
+		assertStringIsNotNull(_moneyDetails.delete(100));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#delete(int)} kein
+	 * Fragezeichen in der Rückgabe enthält.
+	 */
+	@Test
+	public void testDeleteHasNoQuery() {
+		assertEquals(0, frequency(_moneyDetails.delete(100), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#delete(int)} die
+	 * übergebene ID in der Rückgabe enthält.
+	 */
+	@Test
+	public void testDeleteHasId() {
+		assertEquals(1, frequency(_moneyDetails.delete(100), "100"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#delete(int)} eine
+	 * leere Zeichenkette liefert, wenn der Parameter <b>-1</b> ist.
+	 */
+	@Test
+	public void testDeleteWithNullAsParameterReturnNull() {
+		assertStringIsNotNull(_moneyDetails.delete(-1));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#delete(int)} ein
+	 * Fragezeichen in der Rückgabe enthält, wenn der Parameter <b>-1</b> ist.
+	 */
+	@Test
+	public void testDeleteWithNullAsParameterHasNoQuery() {
+		assertEquals(1, frequency(_moneyDetails.delete(-1), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#delete(int)} keine
+	 * ID in der Rückgabe enthält, wenn der Parameter <b>-1</b> ist.
+	 */
+	@Test
+	public void testDeleteWithNullAsParameterHasId() {
+		assertEquals(0, frequency(_moneyDetails.delete(-1), "100"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#update(int)} keine leere
+	 * Zeichenkette liefert.
+	 */
+	@Test
+	public void testUpdateReturnNotNull() {
+		assertStringIsNotNull(_moneyDetails.update(100));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#update(int)} drei
+	 * Fragezeichen in der Rückgabe enthält.
+	 */
+	@Test
+	public void testUpdateHasThreeQuery() {
+		assertEquals(3, frequency(_moneyDetails.update(100), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#update(int)} die
+	 * übergebene ID in der Rückgabe enthält.
+	 */
+	@Test
+	public void testUpdateHasId() {
+		assertEquals(1, frequency(_moneyDetails.update(100), "100"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#update(int)} keine leere
+	 * Zeichenkette liefert, auch wenn als Parameter -1 übergeben wird
+	 */
+	@Test
+	public void testUpdateMinusOneAsParameterReturnNotNull() {
+		assertStringIsNotNull(_moneyDetails.update(-1));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#update(int)} vier
+	 * Fragezeichen in der Rückgabe enthält, wenn der PArameter -1 ist.
+	 */
+	@Test
+	public void testUpdateMinusOneAsParameterHasForeQuery() {
+		assertEquals(4, frequency(_moneyDetails.update(-1), "?"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#update(int)} keine
+	 * ID in der Rückgabe enthält, wenn der Parameter -1 ist.
+	 */
+	@Test
+	public void testUpdateMinusOneAsParameterHasNoId() {
+		assertEquals(0, frequency(_moneyDetails.update(-1), "-1"));
+	}
+	
+	/**
+	 * Testet, ob die Methode {@link db.query.MoneyDetails#select()} keine
+	 * leere Zeichenkette liefert.
+	 */
+	@Test
+	public void testSelectReturnNotNull() {
+		assertStringIsNotNull(_moneyDetails.select());
+	}
 }
