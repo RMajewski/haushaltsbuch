efe1c6e26222f35b9e0b4dcfc70e6074cfbdac30
René Majewski <rene.majewski@gmx.de>
2016-04-12 17:07:46 +0200

    Lizenz in alle Dateien eingefügt.

--- src/dialogs/DlgAbout.java
+++ src/dialogs/DlgAbout.java
@@ -1,3 +1,22 @@
+/* 
+* Copyright 2016 René Majewski
+*  
+* Lizenziert unter der EUPL, Version 1.1 oder - sobald diese von der
+* Europäischen Kommission genehmigt wurden - Folgeversionen der EUPL
+* ("Lizenz"); Sie dürfen dieses Werk ausschließlich gemäß dieser Lizenz
+* nutzen. 
+* 
+* Eine Kopie der Lizenz finden Sie hier: 
+* https://joinup.ec.europa.eu/software/page/eupl
+*  
+* Sofern nicht durch anwendbare Rechtsvorschriften gefordert oder in 
+* schriftlicher Form vereinbart, wird die unter der Lizenz verbreitete 
+* Software "so wie sie ist", OHNE JEGLICHE GEWÄHRLEISTUNG ODER BEDINGUNGEN -
+* ausdrücklich oder stillschweigend - verbreitet.
+* Die sprachspezifischen Genehmigungen und Beschränkungen unter der Lizenz
+* sind dem Lizenztext zu entnehmen.
+*/ 
+
 package dialogs;
 
 import java.awt.BorderLayout;
@@ -48,7 +67,7 @@ public class DlgAbout extends JDialog {
 		JPanel pan = new JPanel();
 		pan.add(new JLabel("Haushaltsbuch"));
 		pan.add(new JLabel("Version 0.1"));
-		pan.add(new JLabel("Copyright © by René Majewski"));
+		pan.add(new JLabel("Copyright René Majewski"));
 		pan.add(new JLabel("http://github.com/RMajewski/haushaltsbuch"));
 		add(pan, BorderLayout.CENTER);
 		
